5c5cc5af3daf25eabf56aaa96695e2d746ec6d10
zhaixu-dong <2330841559@qq.com>
2022-07-15 01:45:26 +0800

    测试修改git

--- src/main/java/Thread/MyThread.java
+++ src/main/java/Thread/MyThread.java
@@ -4,7 +4,7 @@ public class MyThread extends Thread{
 
     @Override
     public void run(){
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             try {
                 ///Thread.sleep(100);
                 System.out.println(Thread.currentThread().getName() + "正在运行" + i);
@@ -18,7 +18,7 @@ public class MyThread extends Thread{
         MyThread mt = new MyThread();
         mt.start();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName() + "正在运行" + i );
         }
     }
